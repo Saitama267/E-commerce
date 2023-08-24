1bf744ade64facce13d72afb43c5f243ddb4420b
Saitama267 <marenich2002@gmail.com>
2023-08-24 17:20:35 +0300

    Start of a new Project

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,13 @@
+namespace Core.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        // public string Author { get; set; }
+        // public DateOnly DateOfPublish { get; set; }
+        // public string Publisher { get; set; }
+        // public int NumberOfPages { get; set; }
+
+    }
+}
